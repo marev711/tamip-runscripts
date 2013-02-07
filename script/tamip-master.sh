c1f5809aa1990c12384b0d761e2ab055fc4a6fc8
Martin.Evaldsson <Martin.Evaldsson@smhi.se>
2013-02-07 09:06:51 +0100

    Implementing control flow (cleaning up and reporting an earlier run)

--- script/tamip-master.sh
+++ script/tamip-master.sh
@@ -1,4 +1,4 @@
-#! /bin/sh -
+#! /bin/sh -eu
  
 #########################
 # 
@@ -14,6 +14,13 @@
 #
 ########################
 
+function mail_report()
+{
+    subject = $1
+    body_file = $2
+    cat $body_file | mail -s $1 martin.evaldsson@smhi.se 
+}
+
 program=$0
 
 if [[ $0 == */* ]] ; then 
@@ -25,6 +32,8 @@ LOG_DIR=$HOME_DIR/log
 OUTPUT_DIR=$HOME_DIR/data
 BIN_DIR=$HOME_DIR/bin
 SCRIPT_DIR=$HOME_DIR/script
+EXP_DIR=/nobackup/rossby15/rossby/joint_exp/tamip/
+logfile=$LOG_DIR/$(date +%Y%m%d%H).log
 
 function usage {
  echo "Usage: ./tamip-master.sh -cf control-file
@@ -75,4 +84,38 @@ while (( "$#" )); do
   esac  
 done
 
+{
+    log "Start at date=$(date)"
+    log "====================="
 
+    # check if running (sm_maeva)
+    set +e
+    running_jobs=squeue | grep sm_maeva
+    if [ $? -eq 0 ];then
+        log "job is running"
+        log $running_jobs
+        exit 0;
+    fi
+    set -e
+
+    # read running date file
+    . $SCRIPT_DIR/running_job.txt
+
+    # clean up TMIP folder (remove restarts + rename folder)
+    set -x
+    rm -f $EXP_DIR/TMIP/srf*
+    set +x
+    
+    # check for errors and report
+    cd $EXP_DIR
+    cat ece.info NODE* > tamip_email_report.txt
+    cd -
+        
+    # delete running date file
+    # delete running date file entry from control-file
+    # copy next entry of control file to running date file
+    # update template run-scripts
+    # launch next run
+    log "End at date=$(date)"
+    log "====================="
+} >> $logfile 2>&1
