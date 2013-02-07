99ad05dc9fbc5a5c62d13738d7db09e830e816a8
Martin.Evaldsson <Martin.Evaldsson@smhi.se>
2013-02-07 12:43:54 +0100

    Changed which files to report in e-mail

--- script/tamip-master.sh
+++ script/tamip-master.sh
@@ -132,7 +132,7 @@ done
         
         log "Report run report"
         cd $EXP_DIR/TMIP_${running_date}
-        cat ece.info NODE* > $SCRIPT_DIR/tamip_email_report.txt
+        cat ece.info ifs.stat > $SCRIPT_DIR/tamip_email_report.txt
         cd -
         mail_report "T-AMIP_report_${running_date}" $SCRIPT_DIR/tamip_email_report.txt
             
